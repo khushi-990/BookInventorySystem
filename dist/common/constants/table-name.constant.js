d3aeefcbe5dfcdb451e36f445d4679486072c9cc
khushbu.parmar <you@example.com>
2024-10-02 19:36:22 +0530

    Fix: remove unnecessary file

--- dist/common/constants/table-name.constant.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TABLE_NAMES = exports.PROJECT_PREFIX = void 0;
-exports.PROJECT_PREFIX = "table_";
-exports.TABLE_NAMES = {
-    USER: `${exports.PROJECT_PREFIX}user`,
-    BOOK: `${exports.PROJECT_PREFIX}book`,
-};
-//# sourceMappingURL=table-name.constant.js.map
--- dist/common/test/schema.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserModel = void 0;
-const mock_model_1 = require("./mock.model");
-class UserModel extends mock_model_1.MockModel {
-}
-exports.UserModel = UserModel;
-//# sourceMappingURL=schema.model.js.map
